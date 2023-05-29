374aa4cb1f1257585bb6a2ec7ba8888b879c9b7f
amexowubete <amexowubete@gmail.com>
2023-05-29 04:31:20 -0700

    100-print_comb3

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -2,26 +2,26 @@
 
 /**
  * main - prints all possible different combinations of two digits
- * Return: ALways 0 (Success)
+ * the two digit must be different
+ * Return: Always 0
  */
 int main(void)
 {
-	int n, m;
-
-	for (n = 48; n <= 56; n++)
+	int n;
+	int m;
+		for (n = 48; n <= 56; n++)
 	{
 		for (m = n + 1; m <= 57; m++)
-		{
-			if (m || n)
+			if (n || m)
 			{
 				putchar(n);
 				putchar(m);
+				if (n != 56 || m != 57)
 				{
 					putchar(',');
 					putchar(' ');
 				}
 			}
-		}
 	}
 	putchar('\n');
 	return (0);
