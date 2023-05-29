839eb8b9fbb4b86796765c69e849dc997d22f93b
amexowubete <amexowubete@gmail.com>
2023-05-29 00:35:19 -0700

    6-print_numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - prints all single digit numbers of base 10 starting from 0
+ * Return: Always 0
+ */
+int main(void)
+{
+	int n = 48;
+		while (n < 58)
+	{
+		putchar(n);
+		n++;
+	}
+	putchar('\n');
+	return (0);
+}
