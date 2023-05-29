e9e2b088b5de92a7f3922066baa00830aaae41ba
amexowubete <amexowubete@gmail.com>
2023-05-29 02:30:19 -0700

    9-print_comb

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -10,11 +10,11 @@ int main(void)
 		for (n = 48; n <= 57; n++)
 		{
 			putchar(n);
-		}
-		if (n != 57)
-		{
-		putchar(',');
-		putchar(' ');
+			if (n != 57)
+			{
+				putchar(',');
+				putchar(' ');
+			}
 		}
 		putchar('\n');
 		return (0);
