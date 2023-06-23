e4d4adf69bc4bd0b0588fe28da57ae53731d3661
amexowubete <amexowubete@gmail.com>
2023-06-23 00:25:50 -0700

    2-print_strings.c

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -21,8 +21,9 @@ void print_strings(const char *separator, const unsigned int n, ...)
 		printf("(nil)");
 	else
 		printf("%s", str);
-	if (separator != NULL)
+	if (i != (n - 1) && separator != NULL)
 		printf("%s", separator);
 	}
 	printf("\n");
+	va_end(list);
 }
