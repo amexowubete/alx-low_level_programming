3504d7a2468634db8a5c89fa267e00a2abeb9e10
amexowubete <amexowubete@gmail.com>
2023-07-04 06:41:56 -0700

    0-print_listint.c

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -8,12 +8,12 @@
 size_t print_listint(const listint_t *h)
 {
 	size_t a = 0;
+	int n;
 		while (h)
 		{
-			printf("%d\n, h->n);
+			printf("%d\n", h->n);
 			h = h->next;
 			a++;
 		}
 		return (a);
 }
-
