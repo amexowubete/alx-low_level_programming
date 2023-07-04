0f41cbadc64a755d6c23706fc8a5924a9cb4b4ed
amexowubete <amexowubete@gmail.com>
2023-07-04 07:02:19 -0700

    1-listint_len.c

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+
+/**
+ * listint_len - return the number of elements in a linked  listint_t list
+ * @h: pointer to the head
+ * Return: number of elements
+ */
+size_t listint_len(const listint_t *h)
+{
+	size_t num = 0;
+		while (h)
+		{
+			h = h->next;
+			num++;
+		}
+		return (num);
+
