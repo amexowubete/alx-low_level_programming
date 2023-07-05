b6a36d47b7fcec617bb05ff791d6677186dc2d2d
amexowubete <amexowubete@gmail.com>
2023-07-04 19:15:14 -0700

    5-free_listint2.c

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - free listint_t and sets the head to NULl
+ * @head: head of the elemnent
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *tempo;
+		if (head == NULL)
+		return;
+	while (*head != NULL)
+	{
+		tempo = *head;
+		*head = *head->next;
+		free(tempo);
+	}
+	*head = NULL;
+}
