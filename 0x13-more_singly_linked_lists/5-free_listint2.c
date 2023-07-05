f0ecce988317d6a9a9ca969fdf5b1462cdc50ea1
amexowubete <amexowubete@gmail.com>
2023-07-04 19:44:56 -0700

    5-free_listint2.c

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -11,9 +11,9 @@ void free_listint2(listint_t **head)
 		return;
 	while (*head)
 	{
-		tempo = *(head)->next;
-		free(*head);
-		*head = tempo;
+		tempo = *(head);
+		*head = (*head)->next;
+		free(tempo);
 	}
 	*head = NULL;
 }
