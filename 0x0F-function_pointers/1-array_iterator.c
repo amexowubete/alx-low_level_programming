f9c55ff6446f257e3f964c0e7762bb89591fe3d2
amexowubete <amexowubete@gmail.com>
2023-08-23 23:00:47 -0700

    task 1

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "function_pointers"
+/**
+ * array_iterator - a function that execute a function on each element of array
+ * @array: array
+ * @size: how many element to print
+ * @action: pointer to a function
+ */
+void array_iterator(int *array, size_t size, void(*action)(int))
+{
+	unsigned int i;
+		if (action == NULL || array == NULL)
+		return;
+	for (i = 0; i < size; i++)
+	{
+		action(array[i]);
+	}
+}
