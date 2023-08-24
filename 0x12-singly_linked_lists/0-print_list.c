d4e5d3132ec2b3e3437a184b796c5dcd62d99bfa
amexowubete <amexowubete@gmail.com>
2023-08-24 13:43:28 -0700

    0-print_list.c is done

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -11,7 +11,7 @@ list_t print_list(const list_t *h)
 	size_t nodes = 0;
 		while (h)
 	{
-		if (str == NULL)
+		if (h->str == NULL)
 			printf("[0] (nil)\n");
 		else
 			printf("[%u] %s\n, h->len, h->str);
