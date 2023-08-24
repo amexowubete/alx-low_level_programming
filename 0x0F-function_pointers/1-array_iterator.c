bc4214a7013a34ae44daf05ec0ac4fbf34b8be81
amexowubete <amexowubete@gmail.com>
2023-08-23 23:54:17 -0700

    1-array_iterator.c

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -5,14 +5,15 @@
  * @array: array
  * @size: how many element to print
  * @action: pointer to a function
+ * Return: void
  */
 void array_iterator(int *array, size_t size, void(*action)(int))
 {
 	unsigned int i;
-		if (action == NULL || array == NULL)
-		return;
-	for (i = 0; i < size; i++)
-	{
-		(*action)(array[i]));
-	}
+		if (array == NULL || action == NULL)
+			return;
+		for (i = 0; i < size; i++)
+		{
+			action(array[i]);
+		}
 }
