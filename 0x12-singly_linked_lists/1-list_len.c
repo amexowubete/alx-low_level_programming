5b398bcf23c4d991b5a6bb10d898ae3aef7d9c3e
amexowubete <amexowubete@gmail.com>
2023-08-25 05:20:16 -0700

    task 01

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+/**
+ * list_len - the number of elemnets in list_t list
+ * @h: pointer to the head of list
+ * Return: the number of elements in list_t list
+ */
+size_t list_len(const list_t *h)
+{
+	size_t len = 0;
+	while (h)
+	{
+		h = h->next;
+		len++;
+	}
+	return (len);
+}
+
