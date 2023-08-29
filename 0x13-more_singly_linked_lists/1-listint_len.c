64cb075f9b3b5a73e67a8c019e04656a77a5af4f
amexowubete <amexowubete@gmail.com>
2023-08-28 23:36:30 -0700

    1-listint_len.c

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -2,11 +2,11 @@
 #include "lists.h"
 
 /**
- * print_listint - print all the elemnets of listint list
+ * listint_len - print all the elemnets of listint list
  * @h: pointer to the head of the list
  * Return: the number of the nodes
  */
-size_t print_listint(const listint_t *h)
+size_t listint_len(const listint_t *h)
 {
 	size_t number = 0;
 		while (h)
