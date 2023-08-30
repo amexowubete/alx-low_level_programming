fa37df097300fc53070ef66a52aad9a632b9bca8
amexowubete <amexowubete@gmail.com>
2023-08-29 19:21:57 -0700

    102-free_listint_safe.c

---
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -0,0 +1,34 @@
+#include "lists.h"
+/**
+ * free_listint_safe - a function that frees a listint_t list
+ * @h: a double pointer to the first node in the list
+ * Return: the size of the list that was free'd
+ */
+size_t free_listint_safe(listint_t **h)
+{
+	size_t len = 0;
+	int diff;
+	listint_t *tempo;
+		if (!h || !*h)
+		return (0);
+	while (*h)
+	{
+		diff = *h - (*h)->next;
+		if (diff > 0)
+		{
+			tempo = (*h)->next;
+			free(*h);
+			*h = temp;
+			len++;
+		}
+		else
+		{
+			free(*h);
+			*h = NULL;
+			len++;
+			break;
+		}
+	}
+	*h = NULL;
+	return (len);
+}
